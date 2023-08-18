691a47a409e2d3ee35cb04a5d8ac7aab45953e68
Matheus Vespasiano <vespasianomatheus@gmail.com>
2023-08-18 19:43:09 -0300

    testes branch development

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -3,5 +3,5 @@ import * as S from "./styles"
 import { Outlet } from "react-router-dom"
 
 export function Layout() {
-  return <S.Container></S.Container>
+  return <S.Container>aq vamos organizar o layout</S.Container>
 }
