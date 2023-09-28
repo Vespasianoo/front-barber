eabdb017a95adae82e1efda16089a43b279e60df
Matheus Vespasiano <vespasianomatheus@gmail.com>
2023-09-28 16:04:38 -0300

    homepage com rem

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,21 +2,21 @@ import styled from "styled-components"
 
 export const Container = styled.div`
   width: 100%;
-  max-width: 450px;
+  max-width: 28.125rem;
   margin-inline: auto;
-  margin-top: 38px;
-  padding-inline: 12px;
+  margin-top: 2.375rem;
+  padding-inline: 0.75rem;
   @media (min-width: 768px) {
     width: 100%;
     max-width: 1280px;
-    padding-inline: 22px;
+    padding-inline: 1.375rem;
   }
 `
 export const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-bottom: 12px;
+  margin-bottom: 0.75rem;
 
   @media (min-width: 768px) {
     justify-content: space-between;
@@ -26,7 +26,7 @@ export const SocialLinks = styled.ul`
   display: none;
   @media (min-width: 768px) {
     display: flex;
-    gap: 26px;
+    gap: 1.625rem;
     > li svg {
       stroke: ${({ theme }) => theme.colors.primaryColor};
     }
@@ -39,8 +39,8 @@ export const Main = styled.main`
   > div {
     display: flex;
     flex-direction: column;
-    gap: 12px;
-    margin-bottom: 38px;
+    gap: 0.75rem;
+    margin-bottom: 2.375rem;
     > h1 {
       color: ${({ theme }) => theme.colors.textColor2};
       font-size: 1.375rem;
@@ -58,11 +58,11 @@ export const Main = styled.main`
       display: flex;
       place-items: center;
       justify-content: center;
-      gap: 12px;
+      gap: 0.75rem;
 
-      height: 48px;
-      width: min(100%, 240px);
-      padding-inline: 16px;
+      height: 3rem;
+      width: min(100%, 15rem);
+      padding-inline: 1rem;
 
       border-radius: 4px;
       background: ${({ theme }) => theme.colors.primaryColor};
@@ -82,13 +82,13 @@ export const Main = styled.main`
 
   @media (min-width: 768px) {
     display: grid;
-    grid-template-columns: 350px 1fr;
+    grid-template-columns: 21.875rem 1fr;
     place-items: center;
     justify-content: center;
 
     > div {
-      gap: 32px;
-      width: 350px;
+      gap: 2rem;
+      width: 21.875rem;
       > h1 {
         font-size: 1.625rem;
         text-align: initial;
@@ -103,7 +103,7 @@ export const Main = styled.main`
       }
     }
     > img {
-      max-width: 500px;
+      max-width: min(100%, 31.25rem);
       justify-self: end;
     }
   }
@@ -113,16 +113,16 @@ export const Main = styled.main`
     > div {
       width: 100%;
       > h1 {
-        font-size: 36px;
+        font-size: 2.25rem;
       }
       > p {
-        font-size: 20px;
+        font-size: 1.25rem;
       }
     }
   }
 
   @media (min-width: 1280px) {
-    grid-template-columns: 530px 1fr;
+    grid-template-columns: 33.125rem 1fr;
     > div {
       > h1 {
         font-size: 2.5rem;
@@ -132,7 +132,7 @@ export const Main = styled.main`
       }
     }
     > img {
-      width: 550px;
+      width: 34.375rem;
     }
   }
 `
